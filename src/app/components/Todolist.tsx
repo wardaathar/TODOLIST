1a1d95f475b92fcb114120a48e705a9ab1e22bb3
wardaathar <wardaathar1234@gmail.com>
2024-12-20 12:37:06 -0700

    Second Commit

--- src/app/components/Todolist.tsx
+++ src/app/components/Todolist.tsx
@@ -25,34 +25,40 @@ const Todolist: React.FC = () => {
              </header>
             
 
-            <div className='bg-white '> 
-                <main className="flex-grow flex items-center justify-center">
-                <div className="max-w-md mx-auto  text-black p-4 bg-slate-500 rounded-lg shadow-md">
-                    <div className="mb-4">
-                        <div className='flex'>
-                            <h1 className='font-serif'>Todo List</h1>
-                            
-                            <input
-                                type="text"
-                                value={newTodo}
-                                onChange={(e) => setNewTodo(e.target.value)}
-                                placeholder="Add a new todo"
-                            />
+             <div className="bg-white min-h-screen">
+  <main className="flex-grow flex items-center justify-center">
+    <div className="max-w-md mx-auto text-black p-6 bg-slate-500 rounded-lg shadow-md">
+      <div className="mb-4">
+        <h1 className="font-serif text-center text-2xl mb-4">Todo List</h1>
+        <div className="flex space-x-2">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Add a new todo"
+            className="flex-grow p-2 rounded border border-gray-300"
+          />
+          <button
+            className="hover:bg-slate-600 bg-blue-800 text-white px-4 py-2 rounded"
+            onClick={addTodo}
+          >
+            Add
+          </button>
+        </div>
+        <ul className="mt-4 list-disc pl-5">
+          {todos.map((todo, index) => (
+            <li key={index} className="text-white">
+              {todo}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  </main>
+</div>
 
-
-                            <button className='hover:bg-slate-600 bg-blue-800 rounded text-white' onClick={addTodo}>Add</button>
-                            <ul>
-                                {todos.map((todo, index) => (
-                                    <li key={index}>{todo}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                  <div/>
-                </div>
-                </main>
             </div>
-            </div>
+        
 
 
             );
